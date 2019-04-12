e1df78f00a806f013d9cdb2db0f0013cf5deb569
mkeet19 <46718960+mkeet19@users.noreply.github.com>
2019-04-12 11:12:26 -0600

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -4,7 +4,7 @@ if(isset($_POST['submit'])) {
     $subject = $_POST['subject'];
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
-    $mailTo = "khehra.navjit21@gmail.com";
+    $mailTo = "malkeetkaur080592@gmail.com";
     $headers = "From: ".$mailFrom;
     $txt = "You have received E-Mail from ".$name.".\n\n".$message;
     mail($mailTo, $subject, $txt, $headers);
